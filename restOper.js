eb25e4db1aa1173d68b569a5e8b9e4ac4944bc3a
AnsaAli <chinjuansa@example.com>
2024-10-28 11:30:15 +0530

    add rest operator

---
+++ restOper.js
@@ -0,0 +1,26 @@
+//this will treated as individual
+function add(...args){
+   
+ let sum=0;
+ for(const value of args){
+    sum = sum + value
+ }
+ return sum;
+}
+
+let res = add(1,2)
+console.log(res) //3
+
+let res2 = add(1,2,6,4) //i can add multiple args
+console.log(res2) //13
+
+function find(a,b,...args){
+    let multiply = a*b;
+    let sum =0;
+    for(let arg of args){
+        sum +=arg
+    }
+    return [multiply,sum]
+}
+console.log(find(1,2,1,1,1,1)) //[ 2, 4 ]
+ 
